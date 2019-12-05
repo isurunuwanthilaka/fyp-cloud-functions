be8f5b2f48ee5dfeacbfb36b4aa677f1e12bbe93
Isuru Nuwanthilaka <isurunuwanthilaka@gmail.com>
2019-12-05 13:00:19 +0530

    Adding data parameters

--- functions/index.js
+++ functions/index.js
@@ -1,61 +1,66 @@
 const functions = require('firebase-functions');
-// const gcs = require('@google-cloud/storage')();
-// const os = require("os");
-// const path = require("path");
+
 const cors = require("cors")({ origin: true });
 var admin = require("firebase-admin");
 
 var serviceAccount = require("./serviceAccountKey.json");
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://fyp-cloud-83c3b.firebaseio.com"
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://fyp-cloud-83c3b.firebaseio.com"
 });
 var db = admin.database();
 var ref = db.ref("deviceDataStore");
 
- exports.connData = functions.https.onRequest((req, res) => {
-  cors(req, res, () => {
-      const usersRef = ref.child(`${req.body.deviceID}`);
+exports.connData = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+        const usersRef = ref.child(`${req.body.deviceID}`);
 
-      if (req.method !== 'POST') {
-          return res.status(500).json({
-              message: 'Not allowed'
-          })
-      } else {
-          return usersRef.set(
-            req.body
-          ).then(() => {
-              res.status(200).json({
-                  message: req.body
-              });
-              return res.status(200)
-          }).catch(error => {
-              return res.status(500).send(error);
-          })
-      }
-  })
+        if (req.method !== 'POST') {
+            return res.status(500).json({
+                message: 'Not allowed'
+            })
+        } else {
+            return usersRef.set(
+                req.body
+            ).then(() => {
+                res.status(200).json({
+                    message: req.body
+                });
+                return res.status(200)
+            }).catch(error => {
+                return res.status(500).send(error);
+            })
+        }
+    })
 
 });
 
 ///////////////////////////////Cloud Messagging//////////////////////////////////////////////////////////
 
+exports.sendAdminNotification = functions.database.ref('/News/{pushId}').onCreate((snap, context) => {
+    const item = snap.val();
+    if (item.priority === 1) {
+        const payload = {
+            notification: {
+                title: `${item.title}`,
+                body: `${item.description}`
+            },
+            data: {
+                device1ID: `${item.device1ID}`,
+                device1SSID: `${item.device1SSID}`,
+                device2ID: `${item.device2ID}`,
+                device2SSID: `${item.device2SSID}`,
+                fileName: `${item.fileName}`
+            }
+        };
 
-exports.sendAdminNotification = functions.database.ref('/News/{pushId}').onCreate((snap,context) => {
-    const news= snap.val();
-         if(news.priority===1){
-         const payload = {notification: {
-             title:`${news.title}`,
-             body: `${news.description}`
-             }
-         };
-         
-    return admin.messaging().sendToTopic("News",payload)
-        .then(function(response){
-             console.log('Notification sent successfully:',response);
-             return null
-        }) 
-        .catch(function(error){
-             console.log('Notification sent failed:',error);
-        });
-        }
-    });
+        return admin.messaging().sendToTopic("News", payload)
+            .then(function (response) {
+                console.log('Notification sent successfully:', response);
+                return null
+            })
+            .catch(function (error) {
+                console.log('Notification sent failed:', error);
+            });
+    }
+});
